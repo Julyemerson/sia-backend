79c046b658308f5f96aa730f86e258ccb46a2bde
Julyemerson Leonizio <jb.leonizio@gmail.com>
2022-01-04 01:12:47 -0300

    concluido configuração model address

--- app/Models/Address.ts
+++ app/Models/Address.ts
@@ -1,18 +1,31 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, column, belongsTo, BelongsTo } from '@ioc:Adonis/Lucid/Orm'
+import Company from 'App/Models/Company'
 
 export default class Address extends BaseModel {
   @column({ isPrimary: true })
   public id: number
 
   @column()
-  public companyName: string
+  public publicPlace: string
 
   @column()
-  public fantasyName: string
+  public neighborhood: string
 
   @column()
-  public cnpj: string
+  public state: string
+
+  @column()
+  public number: string
+
+  @column()
+  public reference: string
+
+  @column()
+  public companyId: number
+
+  @belongsTo(() => Company)
+  public company: BelongsTo<typeof Company>
 
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
